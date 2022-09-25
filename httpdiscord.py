874facdbf0209e8f6b5e1f5c57e00068d5188e16
Victor <scidotmoney@gmail.com>
2022-09-25 02:58:54 +0000

    discord-bot-trigger/

--- httpdiscord.py
+++ httpdiscord.py
@@ -71,5 +71,5 @@ def dm(user_id):
     
 
 if __name__ == '__main__':
-    #app.run(host='0.0.0.0', port=6666, ssl_context=('fullchain.pem', 'privkey.pem'))
-    app.run(host='0.0.0.0', port=3334)
+    app.run(host='0.0.0.0', port=3334, ssl_context=('fullchain.pem', 'privkey.pem'))
+    # app.run(host='0.0.0.0', port=3334)
